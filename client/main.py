79f6313339eb0f68c90cafe6dcc1b70e4580e999
Felix Salcher <dev.felix.salcher@gmail.com>
2024-03-10 20:09:30 +0100

    updated formatting

--- client/main.py
+++ client/main.py
@@ -5,18 +5,20 @@ import json
 import asyncio
 import requests
 
-async def getIp(): 
-    response = requests.get("https://api.ipify.org")
+
+async def getIp():
+    response = await requests.get("https://api.ipify.org")
     return response.text
 
-async def main(): 
+
+async def main():
     url = "ws://192.168.253.132:3000/stream/test2"
     ip = await getIp()
-    async with connect(url) as websocket: 
-        await websocket.send(json.dumps({ "type": "register:ip", "ip": ip}))
+    async with connect(url) as websocket:
+        await websocket.send(json.dumps({"type": "register:ip", "ip": ip}))
         message = await websocket.recv()
         print(message)
-        while True: 
+        while True:
             pass
 
 if __name__ == "__main__":

708093256a92327f2db56bfdd4156b73b8b41942
Felix Salcher <dev.felix.salcher@gmail.com>
2024-03-17 11:17:01 +0100

    move stepper when holding button

--- server/public/js/video.js
+++ server/public/js/video.js
@@ -8,12 +8,19 @@ const channel = window.location.href.split("/").pop()
 const ws = new WebSocket(`ws://${window.location.host}/stepper/${channel}`)
 
 ws.onopen = () => {
-    buttonUp.addEventListener("click", () => sendMove("y", false))
-    buttonDown.addEventListener("click", () => sendMove("y", true))
-    buttonLeft.addEventListener("click", () => sendMove("x", false))
-    buttonRight.addEventListener("click", () => sendMove("x", true))
+    const speed = 3
+
+    let interval = null
+    buttonUp.addEventListener("mousedown", () => interval = setInterval(() => sendMove("y", speed), 50))
+    buttonUp.addEventListener("mouseup", () => clearInterval(interval))
+    buttonDown.addEventListener("mousedown", () => interval = setInterval(() => sendMove("y", -speed), 50))
+    buttonDown.addEventListener("mouseup", () => clearInterval(interval))
+    buttonLeft.addEventListener("mousedown", () => interval = setInterval(() => sendMove("x", -speed), 50))
+    buttonLeft.addEventListener("mouseup", () => clearInterval(interval))
+    buttonRight.addEventListener("mousedown", () => interval = setInterval(() => sendMove("x", speed), 50))
+    buttonRight.addEventListener("mouseup", () => clearInterval(interval))
 }
 
-function sendMove(stepper, direction) {
-    ws.send(JSON.stringify({"type": "stepper:move", stepper, "amount": direction ? 100 : -100}))
+function sendMove(stepper, amount) {
+    ws.send(JSON.stringify({"type": "stepper:move", stepper, "amount": amount}))
 }
